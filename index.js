bc9b1033e835f175e621bee5ab6a489abb83068d
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-06-12 02:13:36 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = batteryBatches.reduce((accumulator,batchCount)=>{ // The initial variable comes first in the list of argmnats
+    return accumulator += batchCount
+},0);
